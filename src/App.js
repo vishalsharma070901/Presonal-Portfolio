8e8dc5d461bd5a216d907f30ba9a759f99cbb3bf
vishalsharma070901 <shobits723@gmail.com>
2023-05-10 19:47:40 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import HashLoader from "react-spinners/HashLoader";
+import Countup from "./components/Countup";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+// import ParticleBack from "./components/ParticleBack";
+
+
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setInterval(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <HashLoader className="loder"
+          color="#4ec2ec"
+          size={100}
+          cssOverride={{}}
+         style={{color:'red'}}/>
+      ) : (
+        <div>
+            {/* <ParticleBack/> */}
+          <Header />
+          <Main/>
+          <About/>
+           <Countup/>
+           <Education/>
+          <Skills/>   
+          <Project/>
+          <Contact/>  
+           <Footer/>  
+         
+            
+         
+        </div>
+      )}
     </div>
   );
 }
